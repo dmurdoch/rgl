77988bd52d25402d94cbc7a248665660e6c410ce
Daniel Adler <dadler@dyncall.org>
2003-06-03 07:51:56 +0000

    updated connect(), allow to use different displayname
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@33 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- src/x11gui.cpp
+++ src/x11gui.cpp
@@ -1,7 +1,7 @@
 // C++ source
 // This file is part of RGL.
 //
-// $Id: x11gui.cpp,v 1.1 2003/03/25 00:13:21 dadler Exp $
+// $Id: x11gui.cpp,v 1.2 2003/06/03 07:51:56 dadler Exp $
 
 #include "x11gui.h"
 #include "lib.h"
@@ -238,11 +238,11 @@ void X11GUIFactory::throw_error(const char* string)
 // connect
 //
 
-void X11GUIFactory::connect()
+void X11GUIFactory::connect(const char* displayname)
 {
   // open one display connection for all RGL X11 devices
   
-  xdisplay = XOpenDisplay(NULL);
+  xdisplay = XOpenDisplay(displayname);
   
   if (xdisplay == NULL) {
     throw_error("unable to open display"); return;
@@ -378,7 +378,7 @@ X11GUIFactory::X11GUIFactory(const char* displayname)
   xvisualinfo = NULL;
   glxctx      = NULL;
 
-  connect(); 
+  connect(displayname); 
 }
 
 
