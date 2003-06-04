385786990f003cdb9f05f59b7076ef80b8a7e8df
Daniel Adler <dadler@dyncall.org>
2003-06-04 07:46:44 +0000

    fixed .dynLibs() string and moved rgl.quit() to plugin.R source file
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@39 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- R/zzz.R
+++ R/zzz.R
@@ -2,13 +2,34 @@
 ## R source file
 ## This file is part of rgl
 ##
-## $Id: zzz.R,v 1.3 2003/05/14 10:58:36 dadler Exp $
+## $Id: zzz.R,v 1.4 2003/06/04 07:46:44 dadler Exp $
 ##
 
 ##
 ## ===[ SECTION: package entry/exit point ]===================================
 ##
 
+##
+## unload DLL
+## 
+
+.rgl.unload.dll <- function()
+{
+  # unload dll
+  
+  dyn.unload( system.file( "libs", paste( "rgl", .Platform$dynlib.ext, sep=""), package="rgl" ) )
+    
+  # fix .DynLibs() string
+    
+  libs <- .dynLibs()
+
+  index <- match("rgl",libs)
+  
+  if ( !is.na(index) ) {
+    libs <- libs[-index]
+    .dynLibs(libs)
+  }
+}
 
 ##
 ## entry-point
@@ -30,7 +51,7 @@
   )
   
   if (!ret$success) {
-    dyn.unload( file.path( libpath, "libs", paste( "rgl", .Platform$dynlib.ext, sep="") ) )
+    .rgl.unload.dll() 
     stop("error rgl_init")
   }
   
@@ -43,25 +64,13 @@
 ##
 
 .Last.lib <- function(libpath)
-{
+{ 
   # shutdown
   
   ret <- .C( symbol.C("rgl_quit"), 
     success=FALSE, 
   )
   
-  if (!ret$success)
-    stop("error rgl_quit")
-  
-  # unload shared library
-
-  dyn.unload( file.path( libpath, "libs", paste( "rgl", .Platform$dynlib.ext, sep="") ) )
-
-  # R BUG: i must fix .Dyn.libs environment variable manually
-  # the variable is used by 'library.dynam' to determine if a package is unloaded
-  # workaround: find and remove the string item "rglview" from the list manually
-
-  # .Dyn.libs <- get(".Dyn.libs", envir=NULL)
-  # .Dyn.libs <- .Dyn.libs[-match( "rgl", .Dyn.libs )]
-  # assign(".Dyn.libs", .Dyn.libs, envir=NULL)
+  .rgl.unload.dll()
 }
+
