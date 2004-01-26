e4044bdcf899146dffdeabeb5b1918e0caa93cd4
Daniel Adler <dadler@dyncall.org>
2004-01-26 07:32:44 +0000

    exchanged sub with gsub as of in G5 using MacOS X 'Panther' the /user/X11R6/lib appears twice.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@99 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 ## R source file
 ## This file is part of rgl
 ##
-## $Id: zzz.R,v 1.4 2003/06/04 07:46:44 dadler Exp $
+## $Id: zzz.R,v 1.5 2004/01/26 07:32:44 dadler Exp $
 ##
 
 ##
@@ -40,7 +40,7 @@
 {
   # For MacOS X we have to remove /usr/X11R6/lib from the DYLD_LIBRARY_PATH
   # because it would override Apple's OpenGL framework
-  Sys.putenv("DYLD_LIBRARY_PATH"=sub("/usr/X11R6/lib","",Sys.getenv("DYLD_LIBRARY_PATH")))
+  Sys.putenv("DYLD_LIBRARY_PATH"=gsub("/usr/X11R6/lib","",Sys.getenv("DYLD_LIBRARY_PATH")))
 
   # load shared library
   
