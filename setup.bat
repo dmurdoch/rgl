4c306da839f591eef3a89c85f01dca598f8882c8
Daniel Adler <dadler@dyncall.org>
2004-02-27 17:34:39 +0000

    removed configure.win copying
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@104 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- setup.bat
+++ setup.bat
@@ -1,7 +1,8 @@
 @echo off
-REM  RGL windows setup build tool
+REM RGL windows build tool setup
 REM This file is part of the RGL software
 REM (c) 2003 D.Adler
+REM $Id: setup.bat,v 1.6 2004/02/27 17:34:39 dadler Exp $
 
 set SRC=src
 
@@ -25,14 +26,12 @@ REM === build tool: mingw ====================================================
 
 :mingw
 echo include build/mingw/Makefile >%SRC%\Makefile.win
-copy src\build\mingw\configure.win configure.win
 goto done
 
 REM === build tool: vc =======================================================
 
 :vc
 echo include build/vc/Makefile >%SRC%\Makefile.win
-copy src\build\vc\configure.win configure.win
 goto done
 
 
