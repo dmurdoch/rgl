8f407d35a355366744862102e39c68ba905d94f4
Daniel Adler <dadler@dyncall.org>
2004-09-17 13:28:30 +0000

    added a more clean 'AutoDestroy'-alike class.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@219 79a1b648-8b68-254f-b20b-c0b50f5137bf

---
+++ src/Disposable.hpp
@@ -0,0 +1,55 @@
+#ifndef RGL_DISPOSABLE_HPP
+#define RGL_DISPOSABLE_HPP
+
+#include <vector>
+
+// forward declaration
+class Disposable;
+
+/**
+ * Dispose Listener Interface.
+ **/
+struct IDisposeListener
+{
+  virtual void notifyDisposed(Disposable* disposing) = 0;
+};
+
+/**
+ * Disposable objects can be disposed autonomous while
+ * they might be managed by a different Subject.
+ * They allow for registering listeners that will be notified when
+ * the object becomes disposed.
+ **/ 
+class Disposable
+{
+public:
+  /**
+   * register listener. It is not allowed to add the listener
+   * if it is already registered.
+   * 
+   * @arg l listener to be added to list
+   **/
+  void addDisposeListener(IDisposeListener* l);
+  /**
+   * unregister listener.
+   * @arg l listener to be removed
+   **/
+  void removeDisposeListener(IDisposeListener* l);
+  /**
+   * dispose object. 
+   * Default implementatin will fire NotifyDisposed.
+   **/
+  virtual void dispose();
+protected:
+  /**
+   * fires 'notifyDisposed' on all registered listeners.
+   * It is save to call add/remove during 'notifyDisposed'.
+   **/
+  void fireNotifyDisposed();
+private:
+  typedef std::vector<IDisposeListener*> Container;
+  Container disposeListeners;
+};
+
+#endif // RGL_DISPOSABLE_HPP
+
