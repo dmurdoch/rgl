5803829051dfa268e00dd35a057671f6eb5ac25c
Duncan Murdoch <murdoch.duncan@gmail.com>
2005-07-22 18:41:04 +0000

    LineStrips use 1 vertex per element, not 2
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@283 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- src/LineStripSet.cpp
+++ src/LineStripSet.cpp
@@ -7,7 +7,7 @@
 //
 
 LineStripSet::LineStripSet(Material& in_material, int in_nvertices, double* in_vertex)
-  : PrimitiveSet(in_material, in_nvertices, in_vertex, GL_LINE_STRIP, 2)
+  : PrimitiveSet(in_material, in_nvertices, in_vertex, GL_LINE_STRIP, 1)
 {
   material.lit = false;
 }
