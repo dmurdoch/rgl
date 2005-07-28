2b397523510183588c274d7b3058001646a575bf
Duncan Murdoch <murdoch.duncan@gmail.com>
2005-07-28 17:10:56 +0000

    Create default methods for transformation generics
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@323 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- R/matrices.R
+++ R/matrices.R
@@ -25,12 +25,53 @@ rotationMatrix <- function(angle,x,y,z,matrix)
         matrix[3,1] <- matrix[3,1] - sina*u[2]
         matrix[3,2] <- matrix[3,2] + sina*u[1]
     }
-    cbind(rbind(matrix,c(0,0,0)),c(0,0,0,1))
+    if (all.equal(dim(matrix), c(3,3))) 
+        matrix <- cbind(rbind(matrix,c(0,0,0)),c(0,0,0,1))
+    return(matrix)    
 }
 
 # Coordinate conversions
 
-asHomogeneous <- function(x) c(x,1)
+asHomogeneous <- function(x) {
+    if (is.matrix(x) && dim(x)[2] == 3) return(cbind(x,1))
+    else if (length(x) == 3) return(c(x,1))
+    else stop("Object is not row vector(s)")
+}
 
-asEuclidean <- function(x) c(x[1]/x[4],x[2]/x[4],x[3]/x[4])
+asEuclidean <- function(x) {
+    if (is.matrix(x) && dim(x)[2] == 4) return(x[,1:3]/x[,4])
+    else if (length(x) == 4) return(c(x[1]/x[4],x[2]/x[4],x[3]/x[4]))
+    else stop("Object is not row vectors(s)")
+}
 
+# Default implementations of transformations
+
+translate3d.default <- function(obj,x,y,z,...) {
+    if (is.matrix(obj)) n <- dim(obj)[1]
+    else n <- 1
+        
+    if (length(obj) == 3 || (is.matrix(obj) && dim(obj)[2] == 3))
+    	return(obj + cbind(rep(x,n), rep(y,n), rep(z,n)))
+    else if (length(obj) == 4 || (is.matrix(obj) && dim(obj)[2] == 4))
+	return(obj %*% translationMatrix(x,y,z))
+    else stop("Unsupported object for translation")
+}
+	
+scale3d.default <- function(obj,x,y,z,...){
+    if (is.matrix(obj)) n <- dim(obj)[1]
+    else n <- 1
+        
+    if (length(obj) == 3 || (is.matrix(obj) && dim(obj)[2] == 3))
+    	return(obj * cbind(rep(x,n), rep(y,n), rep(z,n)))
+    else if (length(obj) == 4 || (is.matrix(obj) && dim(obj)[2] == 4))
+    	return(obj %*% scaleMatrix(x,y,z))
+    else stop("Unsupported object for scaling")
+}
+
+rotate3d.default <- function(obj,angle,x,y,z,matrix,...) {
+    if (length(obj) == 3 || (is.matrix(obj) && dim(obj)[2] == 3))
+    	return(asEuclidean(asHomogeneous(obj) %*% rotationMatrix(angle,x,y,z,matrix)))
+    else if (length(obj) == 4 || (is.matrix(obj) && dim(obj)[2] == 4))
+    	return(obj %*% rotationMatrix(angle,x,y,z,matrix))
+    else stop("Unsupported object for scaling")
+}    
