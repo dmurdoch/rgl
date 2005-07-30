99d21e3824974638fb02838ccf03c3cb3875a77c
Daniel Adler <dadler@dyncall.org>
2005-07-30 09:24:42 +0000

    cleaned up demos: - Uses r3d functions now. - Removed duplicates/preview code such as (qmesh/subdivison etc.) - Moved demo parts into functions, to have less globals.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@331 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- demo/envmap.r
+++ demo/envmap.r
@@ -1,6 +1,20 @@
-data(volcano)
-surface3d( 10*1:nrow(volcano),10*1:ncol(volcano),5*volcano
- , texture=system.file("textures/refmap.png",package="rgl")
- , texenvmap=TRUE
-)
+# RGL-Demo: environment mapping
+# Author: Daniel Adler
+# $Id$
+
+rgl.demo.envmap <- function()
+{
+  # Clear scene:
+  clear3d()
+  bg3d(sphere=T, color="white", back="filled"
+  , texture=system.file("textures/refmap.png",package="rgl")
+  )
+  data(volcano)
+
+  surface3d( 10*1:nrow(volcano),10*1:ncol(volcano),5*volcano
+  , texture=system.file("textures/refmap.png",package="rgl")
+  , texenvmap=TRUE
+  )
+}
+rgl.demo.envmap()
 
