fd7191c6213257eaddd295800b07b193532ba356
Daniel Adler <dadler@dyncall.org>
2005-07-31 13:02:05 +0000

    merge: par3d C++/R include fix
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/branches/dadler-x11fix@343 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- src/par3d.cpp
+++ src/par3d.cpp
@@ -1,9 +1,7 @@
 /* Avoid conflict with Rinternals.h */
-#undef DEBUG
+// #undef DEBUG
 
-#include <Rdefines.h>
 #include <R.h>
-#include <Rinternals.h>
 
 #include "api.h"
 
@@ -13,9 +11,10 @@
 #define streql(s, t)	(!strcmp((s), (t)))
 
 extern "C" {
+#include <Rdefines.h>
+#include <Rinternals.h>
 EXPORT_SYMBOL SEXP par3d(SEXP args);
 }
-
 /* par3d implementation based on R's par implementation
  *
  *  Main functions:
@@ -267,3 +266,5 @@ SEXP par3d(SEXP args)
     }
     return value;
 }
+
+
