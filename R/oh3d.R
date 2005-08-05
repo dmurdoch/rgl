6e04bc1f644d94ad077cf4a501cb7b37c08b02e8
Duncan Murdoch <murdoch.duncan@gmail.com>
2005-08-05 13:19:38 +0000

    cube3d() and oh3d() should not open graphics windows
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@386 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- R/oh3d.R
+++ R/oh3d.R
@@ -92,5 +92,9 @@ oh3d.ib <- c(
     7, 23, 27, 11  
 )
 
-oh3d <- function(trans = par3d("userMatrix")) rotate3d(qmesh3d( oh3d.vb, oh3d.ib ), matrix=trans)
+oh3d <- function( trans  ) {
+  if ( missing(trans) )
+    trans <- if ( rgl.cur() ) par3d("userMatrix") else diag(4)
+  return(rotate3d(qmesh3d( oh3d.vb, oh3d.ib ), matrix=trans))
+}
 
