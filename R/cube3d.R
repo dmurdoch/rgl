ce3ddf5c3dbdcf5f9a015bad82071e4bd25703ce
Duncan Murdoch <murdoch.duncan@gmail.com>
2005-08-05 13:32:43 +0000

    A better implementation of default transformation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@387 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- R/cube3d.R
+++ R/cube3d.R
@@ -22,8 +22,7 @@ cube3d.ib <- c(
   5, 6, 8, 7      
 )
   
-cube3d <- function( trans  ) {
-  if ( missing(trans) )
-    trans <- if ( rgl.cur() ) par3d("userMatrix") else diag(4)
+cube3d <- function( trans = par3d("userMatrix") ) {
+  if ( missing(trans) && !rgl.cur() ) trans <- diag(4)
   return( rotate3d( qmesh3d( cube3d.vb, cube3d.ib ), matrix = trans) )
 }  
