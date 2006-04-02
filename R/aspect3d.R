cb712ceae64b32ae33d46e4824519bd5715c431b
Duncan Murdoch <murdoch.duncan@gmail.com>
2006-04-02 18:08:58 +0000

    Added control of aspect ratio in plots
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@443 79a1b648-8b68-254f-b20b-c0b50f5137bf

---
+++ R/aspect3d.R
@@ -0,0 +1,19 @@
+aspect3d <- function(x, y = NULL, z = NULL) {
+    if (is.null(y)) {
+    	z <- x[3]
+    	y <- x[2]
+    	x <- x[1]
+    }
+    
+    bbox <- par3d("bbox")
+    scalex <- bbox[2]-bbox[1]
+    scaley <- bbox[4]-bbox[3]
+    scalez <- bbox[6]-bbox[5]
+    
+    avgscale <- sqrt((scalex^2 + scaley^2 + scalez^2)/3)
+        
+    s <- svd(par3d("userMatrix"))
+    
+    par3d(userMatrix = s$u %*% t(s$v) %*% scaleMatrix(x*avgscale/scalex, 
+                                              y*avgscale/scaley, z*avgscale/scalez))    
+}
