e4aa31d96824d91dcfdabc3332298d5f99a9371f
Duncan Murdoch <murdoch.duncan@gmail.com>
2007-10-10 16:46:47 +0000

    Add flag demo
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/trunk@604 79a1b648-8b68-254f-b20b-c0b50f5137bf

---
+++ demo/flag.R
@@ -0,0 +1,25 @@
+
+wave <- function(time) {
+  x <- seq(0,2, len=100)
+  
+  wavefn <- function(x) x * sin(-5*time + 1.5 * (x/2) * 2*pi)/10
+  deriv <- function(x) (wavefn(x + 0.01) - wavefn(x - 0.01))/0.02
+
+  arclen <- cumsum(sqrt(1 + deriv(x)^2))*(x[2]-x[1])
+  keep <- arclen < 2
+  x <- x[keep]
+  y <- matrix(wavefn(x), length(x),20)
+  z <- matrix(seq(0,1, len=20), length(x), 20, byrow=TRUE)
+  arclen <- arclen[keep]
+  
+  par3d(skipRedraw = TRUE)
+  if (nrow(rgl.ids())) rgl.pop()
+  surface3d(x,y,z, texture_s=matrix(arclen/2, length(x), 20), texture_t=z)
+  c(list(skipRedraw = FALSE), spin(time))
+}
+
+open3d()
+material3d(texture = system.file("textures","rgl2.png", package="rgl"), col="white")
+spin <- spin3d(rpm=6,axis=c(0,0,1))
+play3d(wave, 10)
+
