79b8f42e9f87722146e122b11890321a38de4d2b
Duncan Murdoch <murdoch.duncan@gmail.com>
2009-03-26 15:13:52 +0000

    Add addNormals function; fix visibility of shade3d.shapelist3d etc.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@754 79a1b648-8b68-254f-b20b-c0b50f5137bf

---
+++ R/addNormals.mesh3d.R
@@ -0,0 +1,52 @@
+addNormals <- function(x, ...) UseMethod("addNormals")
+  
+addNormals.mesh3d <- function(x, ...) {
+  v <- x$vb
+  
+  # Make sure v is homogeneous with unit w
+  if (nrow(v) == 3) v <- rbind(v, 1)
+  else v <- t( t(v)/v[4,] )
+  
+  normals <- v*0
+  v <- v[1:3,]
+  
+  if (!is.null(x$it)) {
+    it <- x$it
+    for (i in 1:ncol(it)) {
+      normal <- normalize(xprod( v[, it[1, i]] - v[, it[3, i]], 
+                                 v[, it[2, i]] - v[, it[1, i]]))
+      for (j in 1:3) {
+      	if (sum(normals[1:3, it[j,i]]*normal) < 0)
+      	  normals[, it[j,i]] <- normals[, it[j,i]] + c(-normal, 1)
+      	else
+          normals[, it[j,i]] <- normals[, it[j,i]] + c(normal, 1)
+      }
+    }
+  }
+  
+  if (!is.null(x$ib)) {
+    it <- x$ib
+    for (i in 1:ncol(it)) {
+      normal <- normalize(xprod( v[, it[1, i]] - v[, it[4, i]],
+                                 v[, it[2, i]] - v[, it[1, i]]))
+      for (j in 1:4) {
+      	if (sum(normals[1:3, it[j,i]]*normal) < 0)
+      	  normals[, it[j,i]] <- normals[, it[j,i]] + c(-normal, 1)
+      	else
+          normals[, it[j,i]] <- normals[, it[j,i]] + c(normal, 1)
+      }
+
+    }
+  }
+  normals <- t( t(normals)/normals[4,] )
+  x$normals <- normals
+  x
+}
+
+veclen <- function(v) sqrt(sum(v^2))
+
+normalize <- function(v) v/veclen(v)
+
+xprod <- function(v, w) c( v[2]*w[3] - v[3]*w[2],
+                           v[3]*w[1] - v[1]*w[3],
+                           v[1]*w[2] - v[2]*w[1] )
