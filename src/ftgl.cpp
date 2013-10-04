37869ddbd85f9a7ad7d32f97199004b6906a6b6d
Duncan Murdoch <murdoch.duncan@gmail.com>
2013-10-04 17:05:52 +0000

    FTGL renaming didn't work
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@978 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- src/ftgl.cpp
+++ src/ftgl.cpp
@@ -1,27 +1,6 @@
 
 #ifdef HAVE_FREETYPE
 
-// We remap the names to avoid conflicts
-
-#define FTBuffer 		rgl_FTBuffer
-#define FTBufferFont 		rgl_FTBufferFont
-#define FTBufferFontImpl 	rgl_FTBufferFontImpl
-#define FTBufferGlyph 		rgl_FTBufferGlyph
-#define FTBufferGlyphImpl 	rgl_FTBufferGlyphImpl
-#define FTCharmap		rgl_FTCharmap
-#define FTFace			rgl_FTFace
-#define FTFont			rgl_FTFont
-#define FTFontImpl		rgl_FTFontImpl
-#define FTGlyph			rgl_FTGlyph
-#define FTGlyphContainer 	rgl_FTGlyphContainer
-#define FTGlyphImpl		rgl_FTGlyphImpl
-#define FTLibrary		rgl_FTLibrary
-#define FTPixmapFont		rgl_FTPixmapFont
-#define FTPixmapFontImpl 	rgl_FTPixmapFontImpl
-#define FTPixmapGlyph		rgl_FTPixmapGlyph
-#define FTPixmapGlyphImpl 	rgl_FTPixmapGlyphImpl
-#define FTSize			rgl_FTSize
-
 #include "FTBuffer.cpp"
 #include "FTCharmap.cpp"
 #include "FTFace.cpp"
