d09e44475cb03fb41dc95bca52fc7870bfb82102
Duncan Murdoch <murdoch.duncan@gmail.com>
2014-04-26 20:00:24 +0000

    Fix initial viewpoint
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/branches/subscenes@1049 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- src/subscene.cpp
+++ src/subscene.cpp
@@ -29,8 +29,8 @@ Subscene::Subscene(Subscene* in_parent, Embedding in_viewport, Embedding in_proj
       projMatrix[4*i + j] = i == j;
     }
   }  
-  if (do_projection > EMBED_INHERIT || do_model > EMBED_INHERIT)
-    add(new Viewpoint);
+  if (parent && (do_projection > EMBED_INHERIT || do_model > EMBED_INHERIT))
+    add(new Viewpoint(*(parent->getViewpoint(true))));
 }
 
 Subscene::~Subscene() 
