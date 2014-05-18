4325bece909e8cd0375bb40a8233da8935f7c80b
Duncan Murdoch <murdoch.duncan@gmail.com>
2014-05-18 14:59:02 +0000

    Fewer Mavericks errors (but still some left...)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1077 79a1b648-8b68-254f-b20b-c0b50f5137bf

---
+++ src/platform.h
@@ -0,0 +1,43 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+/* These are some platform-specific definitions, currently to support MacOSX */
+
+#include "opengl.hpp"
+
+/*  MacOSX */
+
+#ifdef RGL_OSX
+
+#include <Availability.h>
+
+#ifndef __MAC_10_9
+#define __MAC_10_9 1090
+#endif
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_9
+  // pre-Mavericks code
+#else
+  // Mavericks and later
+  
+#define gluProject rgl_gluProject
+#define gluUnProject rgl_gluUnProject
+#define gluErrorString rgl_gluErrorString
+  
+GLint gluProject(GLdouble  objX,  GLdouble  objY,  GLdouble  objZ,  
+                 const GLdouble *  model,  const GLdouble *  proj,  
+                 const GLint *  view,  GLdouble*  winX,  GLdouble*  winY,  
+                 GLdouble*  winZ);
+                 
+GLint gluUnProject(GLdouble  winX,  GLdouble  winY,  GLdouble  winZ,  
+                   const GLdouble *  model,  const GLdouble *  proj,  
+                   const GLint *  view,  GLdouble*  objX,  GLdouble*  objY,  
+                   GLdouble*  objZ);
+                   
+const GLubyte * gluErrorString(GLenum  error);
+                 
+#endif /* Mavericks */
+
+#endif /* RGL_OSX */
+
+#endif /* PLATFORM_H */
