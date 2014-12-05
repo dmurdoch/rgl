493cfcf6c4f5723f11f535884422a94bf1e7c240
Duncan Murdoch <murdoch.duncan@gmail.com>
2014-12-05 14:11:32 +0000

    Fix up table formatting
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1187 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- vignettes/setup.R
+++ vignettes/setup.R
@@ -54,6 +54,16 @@ linkfn <- function(fn, text = paste0("`", fn, "`"), pkg = NA) {
            '</a>')
 }
 
+# Write this once at the start of the document.
+
+cat('<style>
+.nostripes tr.even {background-color: white;}
+table {border-style: none;}
+table th {border-style: none;}
+table td {border-style: none;}
+</style>
+')  
+
 writeIndex <- function(cols = 4) {
   documentedfns <- sort(documentedfns)
   entries <- paste0('<a href="#', documentedfns, '">', documentedfns, '</a>&nbsp;&nbsp;')
@@ -61,5 +71,7 @@ writeIndex <- function(cols = 4) {
   padding <- ((len + cols - 1) %/% cols) * cols - len
   if (padding)
     entries <- c(entries, rep("", length.out=padding))
-  knitr::kable(matrix(entries, ncol=cols), format="pandoc")
+  cat('\n<div class="nostripes">\n')
+  print(knitr::kable(matrix(entries, ncol=cols), format="pandoc"))
+  cat("</div>\n")
 }
