62b98933d01afb74cfb8ab849896453e24e2adc4
Duncan Murdoch <murdoch.duncan@gmail.com>
2015-04-28 15:11:54 +0000

    Add hook_rgl and hook_webgl from knitr.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1242 79a1b648-8b68-254f-b20b-c0b50f5137bf

---
+++ R/Sweave_knitr.R
@@ -0,0 +1,150 @@
+##
+## Sweave device
+##
+##
+
+rgl.Sweave <- function(name, width, height, options, ...) {
+
+  # The 2.13.0 release called dev.off(), not rgl.Sweave.off()
+  
+  if (getRversion() == "2.13.0" && version$status == "") {
+  
+    postscript(tempfile()) # make dev.off() happy
+    if (length(getHook("on.rgl.close"))) rgl.Sweave.off() # to close the previous chunk
+  }
+  
+  if (length(hook <- getHook("on.rgl.close"))) {
+    if (is.list(hook)) hook <- hook[[1]]  # test is for compatibility with R < 3.0.0
+    dev <- environment(hook)$dev
+    rgl.set(dev)
+  } else {
+    wr <- c(0, 0, width*options$resolution, height*options$resolution)
+    open3d(windowRect=wr)
+    if (is.null(delay <- options$delay)) delay <- 0.1
+    Sys.sleep(as.numeric(delay))
+    wrnew <- par3d("windowRect")
+    if (wr[3] - wr[1] != wrnew[3] - wrnew[1] || 
+        wr[4] - wr[2] != wrnew[4] - wrnew[2])
+      stop("rgl window creation error.  Try reducing resolution, width or height.")
+    dev <- rgl.cur()
+  } 
+  
+  snapshotDone <- FALSE
+  
+  stayOpen <- isTRUE(options$stayopen)
+  
+  type <- options$outputtype
+  if (is.null(type)) type <- "png"
+  
+  setHook("on.rgl.close", action="replace", function(remove=TRUE) {
+    prev.dev <- rgl.cur()
+    on.exit(rgl.set(prev.dev))
+    
+    if (!snapshotDone) {
+      rgl.set(dev)
+      switch(type,
+        png = rgl.snapshot(filename=paste(name, "png", sep=".")),
+        pdf = rgl.postscript(filename=paste(name, "pdf", sep="."), fmt="pdf"),
+        eps = rgl.postscript(filename=paste(name, "eps", sep="."), fmt="eps"),
+        stop("Unrecognized rgl outputtype: ", type)
+      )
+      snapshotDone <<- TRUE
+    }
+    
+    if (remove)
+      setHook("on.rgl.close", action="replace", NULL)
+  })
+}
+
+rgl.Sweave.off <- function() {
+  if (length(hook <- getHook("on.rgl.close"))) {
+    if (is.list(hook)) hook <- hook[[1]] # test is for R pre-3.0.0 compatibility
+    stayOpen <- environment(hook)$stayOpen
+    if (stayOpen) hook(FALSE)
+    else rgl.close()
+  }
+}
+  
+##
+## Sweave snapshot
+## 
+##
+
+Sweave.snapshot <- function() {
+  if (length(hook <- getHook("on.rgl.close"))) {
+    if (is.list(hook)) hook <- hook[[1]] # test is for R pre-3.0.0 compatibility
+    hook(remove = FALSE)
+  }
+}
+
+##
+## knitr hook functions
+##
+##
+
+hook_webgl <- local({
+  commonParts <- TRUE
+  reuse <- TRUE
+  function (before, options, envir) 
+  {
+    if (before || rgl.cur() == 0)
+      return()
+    if (requireNamespace("knitr")) { # Should we stop if there is no knitr?
+                                     # We only use it in this test.
+      out_type <- knitr::opts_knit$get("out.format")
+      if (!length(intersect(out_type, c("markdown", "html"))))       
+        stop("hook_webgl is for HTML only.  Use hook_rgl instead.")
+    }  
+    name <- tempfile("webgl", tmpdir = ".", fileext = ".html")
+    on.exit(unlink(name))
+    dpi <- options$dpi / options$fig.retina  # should not consider Retina displays (knitr #901)
+    par3d(windowRect = 100 + dpi * c(0, 0, options$fig.width, 
+                                           options$fig.height))
+    Sys.sleep(.05) # need time to respond to window size change
+    
+    prefix <- gsub('[^[:alnum:]]', '_', options$label) # identifier for JS, better be alnum
+    prefix <- sub('^([^[:alpha:]])', '_\\1', prefix) # should start with letters or _
+
+    res <- writeWebGL(dir = dirname(name), 
+                    filename = name, 
+                    snapshot = !rgl.useNULL(),
+                    template = NULL, 
+                    prefix = prefix, 
+                    commonParts = commonParts,
+                    reuse = reuse)
+    if (!isTRUE(options$rgl.keepopen) && rgl.cur())
+      rgl.close()
+    commonParts <<- FALSE
+    reuse <<- attr(res, "reuse")
+    res <- readLines(name)
+    res = res[!grepl('^\\s*$', res)] # remove blank lines
+    paste(gsub('^\\s+', '', res), collapse = '\n') # no indentation at all (for Pandoc)
+  }
+})
+
+hook_rgl = function(before, options, envir) {
+  # after a chunk has been evaluated
+  if (before || rgl.cur() == 0) return()  # no active device
+  if (!requireNamespace("knitr")) 
+    stop("hook_rgl requires the 'knitr' package.")
+
+  name <- knitr::fig_path('', options)
+  par3d(windowRect = 100 + options$dpi * c(0, 0, options$fig.width, options$fig.height))
+  Sys.sleep(.05) # need time to respond to window size change
+
+  knitr::in_base_dir(save_rgl(name, options$dev))
+
+  options$fig.num = 1L  # only one figure in total
+  knitr::hook_plot_custom(before, options, envir)
+}
+
+save_rgl = function(name, devices) {
+  if (!file_test('-d', dirname(name))) dir.create(dirname(name), recursive = TRUE)
+  # support 3 formats: eps, pdf and png (default)
+  for (dev in devices) switch(
+    dev,
+    postscript = rgl.postscript(paste0(name, '.eps'), fmt = 'eps'),
+    pdf = rgl.postscript(paste0(name, '.pdf'), fmt = 'pdf'),
+    rgl.snapshot(paste0(name, '.png'), fmt = 'png')
+  )
+}
--- vignettes/setup.R
+++ vignettes/setup.R
@@ -1,42 +1,6 @@
 options(rgl.useNULL=TRUE)
 library(rgl)
 
-hook_webgl <- local({
-  commonParts <- TRUE
-  reuse <- TRUE
-  function (before, options, envir) 
-  {
-    if (before || rgl::rgl.cur() == 0 || !requireNamespace("knitr")) 
-      return()
-    out_type <- knitr::opts_knit$get("out.format")
-    if (!length(intersect(out_type, c("markdown", "html"))))       
-      stop("hook_webgl is for HTML only.  Use knitr::hook_rgl instead.")
-    
-    name <- tempfile("webgl", tmpdir = ".", fileext = ".html")
-    on.exit(unlink(name))
-    dpi <- 96 # was options$dpi
-    rgl::par3d(windowRect = dpi * c(0, 0, options$fig.width, 
-                                            options$fig.height))
-    Sys.sleep(0.1)
-    prefix = gsub("[^[:alnum:]]", "_", options$label)
-    prefix = sub("^([^[:alpha:]])", "_\\1", prefix)
-    res <- rgl::writeWebGL(dir = dirname(name), 
-                    filename = name, 
-                    snapshot = !rgl.useNULL(),
-                    template = NULL, 
-                    prefix = prefix, 
-                    commonParts = commonParts,
-                    reuse = reuse)
-    if (!isTRUE(options$rgl.keepopen) && rgl.cur())
-      rgl.close()
-    commonParts <<- FALSE
-    reuse <<- attr(res, "reuse")
-    res <- readLines(name)
-    res <- res[!grepl("^\\s*$", res)]
-    paste(gsub("^\\s+", "", res), collapse = "\n")
-  }
-})
-
 knitr::knit_hooks$set(rgl = hook_webgl)
 
 documentedfns <- c()
