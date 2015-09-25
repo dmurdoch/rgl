3bffa45c43aa84309889ff8d273c21d89bb7e6bd
Duncan Murdoch <murdoch.duncan@gmail.com>
2015-09-25 08:37:22 +0000

    Add faceplot3d
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1352 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- R/bgplot3d.R
+++ R/bgplot3d.R
@@ -24,3 +24,89 @@ bgplot3d <- function(expression) {
   }
   invisible(structure(result, value = value))
 }
+
+faceplot3d <- function(expression, 
+		   face = "z-",
+		   line = 0,
+		   reverse = FALSE,
+		   rotate = 0,
+		   x = NULL, y = NULL, z = NULL, 
+		   width = 480,
+		   height = 480,
+		   ignoreExtent = TRUE,
+		   color = "white", specular = "black",
+		   lit = FALSE, 
+		   texmipmap = TRUE,
+		   texminfilter = "linear.mipmap.linear",
+		   texcoords = matrix(c(0,1,1,0,0,0,1,1), ncol=2),
+		   ...) {
+	
+  save <- par3d(ignoreExtent = ignoreExtent)
+  on.exit(par3d(save))
+  
+  stopifnot(width > 0, height > 0)
+  filename <- tempfile(fileext = ".png")
+  png(filename = filename, width=width, height=height,
+      antialias = "subpixel")
+  value <- try(expression)  
+  dev.off()
+  
+  face <- c(strsplit(face, '')[[1]], '-')[1:2]
+  coord <- tolower(face[1])
+  lower <- face[2] == '-'
+  
+  calc <- c(x = is.null(x), y = is.null(y), z = is.null(z))
+  
+  ranges <- .getRanges()
+    switch(coord,
+      x = {
+      	if (is.null(x)) 
+      	  x <- with(ranges, if (lower) 
+      	  	               x[1] - 0.075*line*diff(x)
+      	  	            else
+      	  	               x[2] + 0.075*line*diff(x))
+      	if (is.null(y))
+      	  y <- with(ranges, c(y[1], y[2], y[2], y[1]))
+      	if (is.null(z))
+      	  z <- with(ranges, c(z[1], z[1], z[2], z[2]))
+      },
+      y = {
+      	if (is.null(x)) 
+      		x <- with(ranges, c(x[1], x[2], x[2], x[1]))
+      	if (is.null(y))
+      		y <- with(ranges, if (lower) 
+      			            y[1] - 0.075*line*diff(y)
+      			          else
+      				    y[2] + 0.075*line*diff(y))
+      	if (is.null(z))
+      		z <- with(ranges, c(z[1], z[1], z[2], z[2]))
+      },      	
+      z = {
+      	if (is.null(x)) 
+      		x <- with(ranges, c(x[1], x[2], x[2], x[1]))
+      	if (is.null(y))
+      		y <- with(ranges, c(y[1], y[1], y[2], y[2]))
+      	if (is.null(z))
+      		z <- with(ranges,  if (lower) 
+      			             z[1] - 0.075*line*diff(z)
+      			           else
+      				     z[2] + 0.075*line*diff(z))
+      }) 
+  x <- cbind(x, y, z)  
+  if (nrow(x) != 4) 
+    stop("Exactly 4 corners must be specified.")
+  
+  if (reverse) {
+    temp <- x[2,]
+    x[2,] <- x[4,]
+    x[4,] <- temp
+  }
+  if (rotate) 
+    x <- x[(0:3 + rotate) %% 4 + 1, ]
+  
+  result <- quads3d(x, texture=filename, texcoords = texcoords,
+  		  color = color, lit = lit, texmipmap = texmipmap, 
+  		  texminfilter = texminfilter, ...)
+  invisible(structure(result, value = value, xyz = x, texcoords = texcoords,
+  		      filename = filename))
+}
