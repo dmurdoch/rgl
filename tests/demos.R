1f78b0e51f9eeb6b16819e5e47401d7ec2d7e909
Duncan Murdoch <murdoch.duncan@gmail.com>
2020-02-04 11:03:10 +0000

    Fix issue with high colour depth
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1720 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- tests/demos.R
+++ tests/demos.R
@@ -3,7 +3,8 @@ library(rgl)
 # regression :  this failed for headless tests
 par3d(userMatrix = diag(4))
 
-for(demo in demo(package="rgl")$results[,"Item"]) 
-  if (!(demo %in% c("rgl", "lsystem", "rglExamples", 
-  		    "shinyDemo", "simpleShinyRgl")))
-    demo(demo, package="rgl", character.only=TRUE)
+if (!rgl.useNULL())
+  for(demo in demo(package="rgl")$results[,"Item"]) 
+    if (!(demo %in% c("rgl", "lsystem", "rglExamples", 
+  	            	    "shinyDemo", "simpleShinyRgl")))
+      demo(demo, package="rgl", character.only=TRUE)
