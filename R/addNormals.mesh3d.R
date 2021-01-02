7a40551857ffc3c8e2089177d22efe0eacde6abe
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-01-02 15:53:16 +0000

    Fix bugs in addNormals; add angleWeighted argument
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/rgl/pkg/rgl@1864 79a1b648-8b68-254f-b20b-c0b50f5137bf

--- R/addNormals.mesh3d.R
+++ R/addNormals.mesh3d.R
@@ -1,45 +1,53 @@
 addNormals <- function(x, ...) UseMethod("addNormals")
   
-addNormals.mesh3d <- function(x, ...) {
+addNormals.mesh3d <- function(x, angleWeighted = TRUE, ...) {
   v <- x$vb
   
   # Make sure v is homogeneous with unit w
   if (nrow(v) == 3) v <- rbind(v, 1)
   else v <- t( t(v)/v[4,] )
   
-  normals <- v*0
   v <- v[1:3,]
+  normals <- v*0
   
-  if (length(x$it)) {
-    it <- x$it
+  if (is.na(angleWeighted)) {
+    reproduceBug <- TRUE
+    angleWeighted <- FALSE
+  } else
+    reproduceBug <- FALSE
+  
+  dopolys <- function(it, normals) {
+    n <- nrow(it)
     for (i in seq_len(ncol(it))) {
-      normal <- normalize(xprod( v[, it[1, i]] - v[, it[3, i]], 
-                                 v[, it[2, i]] - v[, it[1, i]]))
-      if (!any(is.na(normal)))
-        for (j in 1:3) {
-          if (sum(normals[1:3, it[j,i]]*normal) < 0)
-      	    normals[, it[j,i]] <- normals[, it[j,i]] + c(-normal, 1)
-      	  else
-            normals[, it[j,i]] <- normals[, it[j,i]] + c(normal, 1)
+      normal <- xprod( v[, it[1, i]] - v[, it[3, i]], 
+                       v[, it[2, i]] - v[, it[1, i]])
+      if (reproduceBug)
+        normal <- normalize(normal)
+      if (!any(is.na(normal))) {
+        if (angleWeighted) 
+          normal <- normalize(normal)
+        
+        for (j in seq_len(n)) {
+          if (angleWeighted) {
+            jm1 <- (j + n - 2) %% n + 1
+            jp1 <- j %% n + 1
+            weight <- angle(v[, it[jm1, i]] - v[, it[j, i]],
+                           v[, it[jp1, i]] - v[, it[j, i]])
+          } else
+            weight <- 1
+          normals[, it[j,i]] <- normals[, it[j,i]] + normal*weight
         }
+      }
     }
+    normals
   }
   
-  if (length(x$ib)) {
-    it <- x$ib
-    for (i in seq_len(ncol(it))) {
-      normal <- normalize(xprod( v[, it[1, i]] - v[, it[4, i]],
-                                 v[, it[2, i]] - v[, it[1, i]]))
-      if (!any(is.na(normal)))
-        for (j in 1:4) {
-      	  if (sum(normals[1:3, it[j,i]]*normal) < 0)
-      	    normals[, it[j,i]] <- normals[, it[j,i]] + c(-normal, 1)
-      	  else
-            normals[, it[j,i]] <- normals[, it[j,i]] + c(normal, 1)
-        }
-    }
-  }
-  normals <- t( t(normals)/normals[4,] )
+  if (length(x$it)) 
+    normals <- dopolys(x$it, normals)
+  
+  if (length(x$ib)) 
+    normals <- dopolys(x$ib, normals)
+  normals <- rbind(apply(normals, 2, function(n) n/veclen(n)), 1)
   x$normals <- normals
   x
 }
@@ -51,3 +59,8 @@ normalize <- function(v) v/veclen(v)
 xprod <- function(v, w) c( v[2]*w[3] - v[3]*w[2],
                            v[3]*w[1] - v[1]*w[3],
                            v[1]*w[2] - v[2]*w[1] )
+
+angle <- function(a,b) {
+  dot <- sum(a*b)
+  acos(dot/veclen(a)/veclen(b))
+}
