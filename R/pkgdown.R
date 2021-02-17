40ae9fada6bc52ee335a0e0168e1bc9f67394755
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-02-17 16:39:39 -0500

    A start:  inserts the JSON, but doesn't add the assets.

---
+++ R/pkgdown.R
@@ -0,0 +1,15 @@
+in_pkgdown_example <- function() 
+	!is.null(getOption("downlit.rdname")) && 
+	  requireNamespace("downlit") &&
+	  requireNamespace("pkgdown")
+
+pkgdown_print.rglId <- function(x, visible) {
+	rendered <- htmltools::renderTags(rglwidget())
+	result <- rendered$html
+	class(result) <- "rgl_rendered"
+	result
+}
+
+replay_html.rgl_rendered <- function(x, ...) {
+	unclass(x)
+}
