45b0fa6d90d051db57875cbba30a224738f1485a
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-03-18 20:57:00 -0400

    Should work on arbitrary shapes now.

--- R/drape3d.R
+++ R/drape3d.R
@@ -9,42 +9,44 @@ drape3d.mesh3d <- function (obj, x, y = NULL, z = NULL,
 {
     ids <- NULL
     
+    # Takes segment number as input; returns
+    # NULL if in no triangle, otherwise matrix of projected locations and triangle numbers.
     ztri <- function(i){
       p <- psegs[,i]
-        ## Finds triangle with point p inside of it and returns unprojected vector at this point
-        TRI <- array(NA,c(2,2,ncol(obj$it)))
-        for(j in seq_len(ncol(obj$it))){
-             v <- obj$it[,j]       ## vertices of triangle
-             TRI[,,j] <- matrix(c(range(pverts[1,v]),range(pverts[2,v])),2,2)
-        }
-  
-        ## now TRI[,1,i] is x coord range for projected triangle i
-        ## now TRI[,2,i] is y coord range for projected triangle i
-        oo <- (p[1] < TRI[1,1,] | p[1] > TRI[2,1,]) &
-              (p[2] < TRI[1,2,] | p[2] > TRI[2,2,])
-        lam <- numeric(3)
-        for(j in seq_len(ncol(obj$it))[!oo]){
-            ## get barycentric coords of p in projected triangle
-            v <- matrix(pverts[,obj$it[,j]],3,2)  ## v[i,] vertices of projected triangle i
-            D <- (v[2,2]-v[3,2]) * (v[1,1]-v[3,1]) +
-                 (v[3,1]-v[2,1]) * (v[1,2]-v[3,2])
-            if (D == 0) next
-            l <- (v[2,2]-v[3,2]) * (p[1]  -v[3,1]) +
-                 (v[3,1]-v[2,1]) * (p[2]  -v[3,2])
-            lam[1] <- l/D
-            if (lam[1] < 0 || lam[1] > 1) next   ## not in this triangle
-            l <- (v[3,2]-v[1,2]) * (p[1]  -v[3,1]) +
-                 (v[1,1]-v[3,1]) * (p[2]  -v[3,2])
-            lam[2] <- l/D
-            if (lam[2] < 0 || lam[2] > 1) next   ## not in this triangle
-            lam[3] <- 1-sum(lam[1:2])
-            if (lam[3] < 0 || lam[3] > 1) next   ## not in this triangle
-            v <- matrix(verts[obj$it[,j],], 3,3)  ## Now v is vertices of original triangle
-            return(lam %*% v)
-        }
-        NULL
+      TRI <- array(NA,c(2,2,ncol(obj$it)))
+      for(j in seq_len(ncol(obj$it))){
+        v <- obj$it[,j]       ## vertices of triangle
+        TRI[,,j] <- matrix(c(range(pverts[1,v]),range(pverts[2,v])),2,2)
+      }
+      
+      ## now TRI[,1,i] is x coord range for projected triangle i
+      ## now TRI[,2,i] is y coord range for projected triangle i
+      oo <- (p[1] < TRI[1,1,] | p[1] > TRI[2,1,]) &
+        (p[2] < TRI[1,2,] | p[2] > TRI[2,2,])
+      result <- NULL
+      lam <- numeric(3)
+      for(j in seq_len(ncol(obj$it))[!oo]){
+        ## get barycentric coords of p in projected triangle
+        v <- matrix(pverts[,obj$it[,j]],3,2)  ## v[i,] vertices of projected triangle i
+        D <- (v[2,2]-v[3,2]) * (v[1,1]-v[3,1]) +
+          (v[3,1]-v[2,1]) * (v[1,2]-v[3,2])
+        if (D == 0) next
+        l <- (v[2,2]-v[3,2]) * (p[1]  -v[3,1]) +
+          (v[3,1]-v[2,1]) * (p[2]  -v[3,2])
+        lam[1] <- l/D
+        if (lam[1] < 0 || lam[1] > 1) next   ## not in this triangle
+        l <- (v[3,2]-v[1,2]) * (p[1]  -v[3,1]) +
+          (v[1,1]-v[3,1]) * (p[2]  -v[3,2])
+        lam[2] <- l/D
+        if (lam[2] < 0 || lam[2] > 1) next   ## not in this triangle
+        lam[3] <- 1-sum(lam[1:2])
+        if (lam[3] < 0 || lam[3] > 1) next   ## not in this triangle
+        v <- matrix(verts[obj$it[,j],], 3,3)  ## Now v is vertices of original triangle
+        result <- rbind(result, c(lam %*% v, j))
+      }
+      result
     }
-
+    
     obj <- as.tmesh3d(obj)
     nverts <- ncol(obj$vb)
 
@@ -67,11 +69,13 @@ drape3d.mesh3d <- function (obj, x, y = NULL, z = NULL,
       tri[n<-n+1,] <- op(v[c(2,3)])
       tri[n<-n+1,] <- op(v[c(3,1)])
     }
-    tri <- unique(tri)
-    
+
     ## add first segment point assuming not on a triangle edge
     z1 <- ztri(1)
-    if (!is.null(z1)) result <- rbind(result, z1)
+    if (length(z1)) 
+      zs <- cbind(z1, 0) # Entries are triangle no, pt, t in segment
+    else
+      zs <- NULL
 
     for (i in seq_len(ncol(segs))[-1]){
       p1 <- psegs[,i-1]
@@ -85,14 +89,12 @@ drape3d.mesh3d <- function (obj, x, y = NULL, z = NULL,
       }
       if (any(is.na(p1))) {
         ## NAs break line and move to next point
-        result <- rbind(result, c(NA, NA, NA))
         zi <- ztri(i)
-        if (!is.null(zi)) result <- rbind(result, zi)
+        if (length(zi)) 
+          zs <- rbind(zs, cbind(zi, 0))
         next
       }
-      
-      
-      zs <- matrix(NA,nrow=0,ncol=4)
+
       s <- matrix(c(p1,p2),2,2)  ## speedup: winnow futile intersection calcs
       s <- t(apply(s,1,op))
       ## triangle seg x extent is all below or above line seg x extent
@@ -116,23 +118,32 @@ drape3d.mesh3d <- function (obj, x, y = NULL, z = NULL,
         if (t2 < 0 || t2 > 1) next          ## not within p3 ... p4
         v3 <- verts[tri[j,1],]
         v43 <- verts[tri[j,2],] - v3
-        zs <- rbind(zs,c(t1,(v3+t2*v43)))## t1 along line seg & point value
+        k <- (j+2)%/%3   # triangle number
+        zs <- rbind(zs, c(v3+t2*v43, k, t1))## t1 along line seg & point value
       }
-      if (nrow(zs)>0) {
-        ## add points in order of increasing t on segment
-        o <- order(zs[,1])
-        result <- rbind(result, zs[o,-1])
-      }
+      ## add last segment point assuming not on triangle edge
+      zn <- ztri(i)
+      if (!is.null(zn)) 
+        zs <- rbind(zs, cbind(zn, 1))
       
-        ## add last segment point assuming not on triangle edge
-        zn <- ztri(i)
-        if (!is.null(zn)) result <- rbind(result, zn)
-
-        if (plot) 
-            ids <- c(ids, lines3d(result, ...))
+      if (nrow(zs) > 0) {
+        o <- order(zs[,4])
+        zs <- zs[o, , drop = FALSE]
+        ## Only keep cases that are on the same triangle
+        k <- zs[,4]
+        dup <- duplicated(k)
+        nextdup <- c(dup[-1], FALSE)
+        prevdup <- c(FALSE, dup[-length(dup)])
+        keep <- (!dup & nextdup) | # The first for a tri
+                (!prevdup & dup)   # The second
+        zs <- zs[keep,,drop = FALSE]
+        ## add points in order of increasing t on segment
+        o <- order(zs[,4], zs[,5])
+        result <- rbind(result, zs[o,-(4:5)])
+      }
     }
     if (plot)
-      lowlevel(ids)
+      segments3d(result, ...)
     else
       result
 }
