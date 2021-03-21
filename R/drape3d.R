0b795018d453c6c12dcfb528671d7068b0c6db05
dmurdoch <murdoch.duncan@gmail.com>
2021-03-21 10:32:17 -0400

    Add drape3d function (#66)
    * Add line drape functionality: drape3d()
    
    New routine to drape a line across a surface 
    
    Mainly written by ghfbsd <ghfbsd@gmail.com>, with modifications by DJM.

---
+++ R/drape3d.R
@@ -0,0 +1,165 @@
+drape3d <- function(obj, ...) 
+  UseMethod("drape3d")
+
+drape3d.default <- function(obj, ...) 
+  drape3d(as.mesh3d(obj), ...)
+  
+drape3d.mesh3d <- function (obj, x, y = NULL, z = NULL,
+    plot = TRUE, P = cbind(diag(2), 0), ...) 
+{
+    
+    # Takes segment number as input; returns
+    # NULL if in no triangle, otherwise matrix of projected locations and triangle numbers.
+    ztri <- function(i){
+      p <- psegs[,i]
+      TRI <- array(NA,c(2,2,ncol(obj$it)))
+      for(j in seq_len(ncol(obj$it))){
+        v <- obj$it[,j]       ## vertices of triangle
+        TRI[,,j] <- matrix(c(range(pverts[1,v]),range(pverts[2,v])),2,2)
+      }
+      
+      ## now TRI[,1,i] is x coord range for projected triangle i
+      ## now TRI[,2,i] is y coord range for projected triangle i
+      oo <- p[1] < TRI[1,1,] | p[1] > TRI[2,1,] |
+            p[2] < TRI[1,2,] | p[2] > TRI[2,2,]
+      result <- NULL
+      lam <- numeric(3)
+      for(j in which(!oo)){
+        ## get barycentric coords of p in projected triangle
+        v <- pverts[,obj$it[,j]]  ## v[i,] vertices of projected triangle i
+        D <- (v[2,2]-v[2,3]) * (v[1,1]-v[1,3]) +
+          (v[1,3]-v[1,2]) * (v[2,1]-v[2,3])
+        if (D == 0) next
+        l <- (v[2,2]-v[2,3]) * (p[1]  -v[1,3]) +
+          (v[1,3]-v[1,2]) * (p[2]  -v[2,3])
+        lam[1] <- l/D
+        if (lam[1] < 0 || lam[1] > 1) next   ## not in this triangle
+        l <- (v[2,3]-v[2,1]) * (p[1]  -v[1,3]) +
+          (v[1,1]-v[1,3]) * (p[2]  -v[2,3])
+        lam[2] <- l/D
+        if (lam[2] < 0 || lam[2] > 1) next   ## not in this triangle
+        lam[3] <- 1-sum(lam[1:2])
+        if (lam[3] < 0 || lam[3] > 1) next   ## not in this triangle
+        v <- matrix(verts[,obj$it[,j]], 3,3)  ## Now v is vertices of original triangle
+        result <- rbind(result, c(v %*% lam, j))
+      }
+      result
+    }
+    op <- function(v)
+      v[if(v[1] > v[2]) c(2,1) else c(1,2)] ## orders pair
+    
+    obj <- as.tmesh3d(obj)
+
+    verts <- t(asEuclidean(t(obj$vb)))
+    
+    segs <- xyz.coords(x, y, z, recycle=TRUE)
+    segs <- rbind(segs$x, segs$y, segs$z)
+    
+    if (length(dim(P)) != 2 || !all(dim(P) == c(2,3)))
+      stop("P should be a 2 x 3 matrix.")
+    
+    pverts <- P %*% verts  # projected vertices
+    psegs <- P %*% segs # projected segments
+    
+    ## get unique point pairs making a triangle side
+    tri <- matrix(NA,nrow=3*ncol(obj$it),ncol=2)
+    n <- 0
+    for (j in seq_len(ncol(obj$it))) {
+      v <- obj$it[,j]
+      tri[n<-n+1,] <- v[c(1,2)]
+      tri[n<-n+1,] <- v[c(2,3)]
+      tri[n<-n+1,] <- v[c(3,1)]
+    }
+
+    result <- matrix(numeric(), ncol = 3)
+    
+    ## add first segment point assuming not on a triangle edge
+    z1 <- ztri(1)
+    if (length(z1)) 
+      zs <- cbind(z1, 0) # Entries are triangle no, pt, t in segment
+    else
+      zs <- NULL
+
+    for (i in seq_len(ncol(segs))[-1]){
+      p1 <- psegs[,i-1]
+      p2 <- psegs[,i]
+      p21 <- p2 - p1
+      if (any(is.na(p2))) {
+        ## add last segment point assuming not on triangle edge
+        zi <- ztri(i-1)
+        if (!is.null(zi)) zs <- rbind(zs,cbind(zi, 1))
+        next
+      }
+      if (any(is.na(p1))) {
+        ## NAs break line and move to next point
+        zi <- ztri(i)
+        if (length(zi)) 
+          zs <- rbind(zs, cbind(zi, 0))
+        next
+      }
+
+      s <- matrix(c(p1,p2),2,2)  ## speedup: winnow futile intersection calcs
+      s <- t(apply(s,1,op))
+      ## triangle seg x extent is all below or above line seg x extent
+      sx <- (pverts[1,tri[,1]] < s[1,1] & pverts[1,tri[,2]] < s[1,1]) |
+        (pverts[1,tri[,1]] > s[1,2] & pverts[1,tri[,2]] > s[1,2])
+      ## triangle seg y extent is all below or above line seg y extent
+      sy <- (pverts[2,tri[,1]] < s[2,1] & pverts[2,tri[,2]] < s[2,1]) |
+        (pverts[2,tri[,1]] > s[2,2] & pverts[2,tri[,2]] > s[2,2])
+      for(j in seq_len(nrow(tri))[!sx & !sy]){     ## possible intersections
+        p3 <- pverts[,tri[j,1]]
+        p4 <- pverts[,tri[j,2]]
+        p43 <- p4-p3
+        p31 <- p3-p1
+        D <- -p21[1]*p43[2] + p21[2]*p43[1]
+        if (D == 0) next                    ## parallel line segs
+        T1<- -p31[1]*p43[2] + p31[2]*p43[1]
+        t1 <- T1/D
+        if (t1 < 0 || t1 > 1) next          ## not within p1 ... p2
+        T2<-  p21[1]*p31[2] - p21[2]*p31[1]
+        t2 <- T2/D
+        if (t2 < 0 || t2 > 1) next          ## not within p3 ... p4
+        v3 <- verts[,tri[j,1]]
+        v43 <- verts[,tri[j,2]] - v3
+        k <- (j+2)%/%3   # triangle number
+        zs <- rbind(zs, c(v3+t2*v43, k, t1))## t1 along line seg & point value
+      }
+      ## add last segment point assuming not on triangle edge
+      zn <- ztri(i)
+      if (!is.null(zn)) 
+        zs <- rbind(zs, cbind(zn, 1))
+      
+      if (length(zs)) {
+        # Order by triangle to group them, then by t
+        # within triangle 
+        o <- order(zs[,4], zs[,5])
+        zs <- zs[o, , drop = FALSE]
+        # Only keep cases that are on the same triangle
+        # Rounding may give us 1 or 3 intersections with a 
+        # triangle; discard singletons, use first and last
+        # for triples.
+        k <- zs[,4]
+        dup <- duplicated(k)
+        nextdup <- c(dup[-1], FALSE)
+        keep <- (!dup & nextdup) | # The first for a triangle
+                (dup & !nextdup)   # The last for one
+        zs <- zs[keep,,drop = FALSE]
+        # Order by t value
+        finish <- 2*seq_len(nrow(zs)/2)
+        start <- finish - 1
+        o <- order(zs[start, 5])
+        start <- start[o]
+        finish <- finish[o]
+        # Drop zero length segments
+        keep <- zs[start, 5] < zs[finish, 5]
+        start <- start[keep]
+        finish <- finish[keep]
+        both <- as.numeric(rbind(start, finish))
+        result <- rbind(result, zs[both, -(4:5), drop = FALSE])
+      }
+    }
+    if (plot)
+      segments3d(result, ...)
+    else
+      result
+}
