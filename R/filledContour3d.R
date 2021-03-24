99ca4e76d48c4d1134768d14dc71a2aee7ba851b
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-03-24 12:00:09 -0400

    Allow filledContour3d to accept levels in decreasing order.

--- R/filledContour3d.R
+++ R/filledContour3d.R
@@ -29,10 +29,20 @@ filledContour3d.mesh3d <- function(obj, fn = "z",
     obj <- subdivision3d(obj, deform = FALSE, normalize = TRUE)
     nverts <- ncol(obj$vb)
   }
+  
   if (is.null(fn))
     fn <- obj$values
   if (is.null(fn))
     stop("'fn' can only be NULL if 'obj' contains values.")
+  
+  if (length(levels) < 2)
+    stop("Must have at least 2 levels.")
+  
+  reverse <- levels[1] > levels[2]
+  
+  if (any((levels[-length(levels)] > levels[-1]) != reverse))
+      stop("Levels must be monotone increasing or decreasing")
+
   verts <- asEuclidean(t(obj$vb))
   if (is.numeric(fn))
     values <- fn
@@ -45,9 +55,12 @@ filledContour3d.mesh3d <- function(obj, fn = "z",
   obj$material$color <- NA
   obj$values <- values
   result <- list()
-  for (i in seq_along(col)) {
-    obj <- clipMesh3d(obj, NULL, bound = levels[i], keepValues = TRUE)
-    strip <- clipMesh3d(obj, NULL, bound = levels[i+1], greater = FALSE, keepValues = keepValues && !plot)
+  strips <- seq_along(col)
+  if (reverse) strips <- rev(strips)
+  for (i in strips) {
+    r <- range(levels[c(i, i+1)])
+    obj <- clipMesh3d(obj, NULL, bound = r[1], keepValues = TRUE)
+    strip <- clipMesh3d(obj, NULL, bound = r[2], greater = FALSE, keepValues = keepValues && !plot)
     strip$material$color <- col[i]
     result[[i]] <- cleanMesh3d(strip, rejoin = TRUE)
   }
