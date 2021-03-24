0e4440d22b4c726e39e0211eb856b3aac7a37da2
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-03-24 14:52:01 -0400

    Tested too soon

--- R/filledContour3d.R
+++ R/filledContour3d.R
@@ -35,14 +35,6 @@ filledContour3d.mesh3d <- function(obj, fn = "z",
   if (is.null(fn))
     stop("'fn' can only be NULL if 'obj' contains values.")
   
-  if (length(levels) < 2)
-    stop("Must have at least 2 levels.")
-  
-  reverse <- levels[1] > levels[2]
-  
-  if (any((levels[-length(levels)] > levels[-1]) != reverse))
-      stop("Levels must be monotone increasing or decreasing")
-
   verts <- asEuclidean(t(obj$vb))
   if (is.numeric(fn))
     values <- fn
@@ -50,14 +42,22 @@ filledContour3d.mesh3d <- function(obj, fn = "z",
     fn <- .getVertexFn(fn, parent.frame())
     values <- fn(verts)
   }
+  if (length(levels) < 2)
+    stop("Must have at least 2 levels.")
+  
+  reverse <- levels[1] > levels[2]
+  
+  if (any((levels[-length(levels)] > levels[-1]) != reverse))
+    stop("Levels must be monotone increasing or decreasing")
+  
   if (is.null(obj$material))
     obj$material <- list()
   obj$material$color <- NA
   obj$values <- values
   result <- list()
-  strips <- seq_along(col)
-  if (reverse) strips <- rev(strips)
-  for (i in strips) {
+  ivals <- seq_along(col)
+  if (reverse) ivals <- rev(ivals)
+  for (i in ivals) {
     r <- range(levels[c(i, i+1)])
     obj <- clipMesh3d(obj, NULL, bound = r[1], keepValues = TRUE)
     strip <- clipMesh3d(obj, NULL, bound = r[2], greater = FALSE, keepValues = keepValues && !plot)
