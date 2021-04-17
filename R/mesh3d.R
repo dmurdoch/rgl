dmurdoch/rgl

commit 2c76c311dda0311c8bccd1da0974b62794a58a58

Work harder to compare equal to earlier versions. (#86)

DESCRIPTION
@@ -1,5 +1,5 @@
 Package: rgl
-Version: 0.106.5
+Version: 0.106.6
 Title: 3D Visualization Using OpenGL
 Authors@R: c(person("Duncan", "Murdoch", role = c("aut", "cre"),
                      email = "murdoch.duncan@gmail.com"),
@@ -30,6 +30,7 @@ Suggests: MASS, rmarkdown, deldir, orientlib, lattice, misc3d, rstudioapi,
 Imports: graphics, grDevices, stats, utils, 
          htmlwidgets, htmltools, knitr, jsonlite (>= 0.9.20), shiny,
          magrittr, crosstalk, manipulateWidget (>= 0.9.0)
+Enhances: waldo
 Description: Provides medium to high level functions for 3D interactive graphics, including
     functions modelled on base graphics (plot3d(), etc.) as well as functions for
     constructing representations of geometric objects (cube3d(), etc.).  Output

R/mesh3d.R
@@ -1,3 +1,9 @@
+ensureMatrix <- function(x, nrow) {
+  if (!is.matrix(x))
+    x <- matrix(x, nrow = nrow)
+  x
+}
+
 mesh3d <- function( x, y = NULL, z = NULL, vertices, 
                     material = NULL,
                     normals = NULL, texcoords = NULL,
@@ -9,6 +15,9 @@ mesh3d <- function( x, y = NULL, z = NULL, vertices,
     xyz <- xyz.coords(x, y, z, recycle=TRUE)
     vertices <- rbind(xyz$x, xyz$y, xyz$z, 1) 
   } else vertices <- asHomogeneous2(vertices)
+  
+  # Remove dimnames
+  dimnames(vertices) <- NULL
 
   if (missing(meshColor) 
       && !is.null(material) 
@@ -16,6 +25,7 @@ mesh3d <- function( x, y = NULL, z = NULL, vertices,
     meshColor <- material$meshColor
     material$meshColor <- NULL
   }
+  
   meshColor <- match.arg(meshColor)
   
   if (is.null(texcoords)
@@ -44,18 +54,46 @@ mesh3d <- function( x, y = NULL, z = NULL, vertices,
   
   object <- list(
     vb = vertices,
-    ip = points,
-    is = if (!is.null(segments)) matrix(segments, nrow = 2),
-    it = if (!is.null(triangles)) matrix(triangles, nrow = 3),
-    ib = if (!is.null(quads)) matrix(quads, nrow = 4),
     material = .getMaterialArgs(material = material),
     normals = normals,
-    texcoords = texcoords,
-    meshColor = meshColor
+    texcoords = texcoords
   ) 
+  
+  if (!is.null(points))    object$ip <- ensureMatrix(points, 1)
+  if (!is.null(segments))  object$is <- ensureMatrix(segments, 2)
+  if (!is.null(triangles)) object$it <- ensureMatrix(triangles, 3)
+  if (!is.null(quads))     object$ib <- ensureMatrix(quads, 4)
 
+  register_compare_proxy()
+  
   class(object) <- c("mesh3d", "shape3d")
   object
+}
+
+register_compare_proxy <- local({
+  registered <- FALSE
+  function() {
+    if (!registered &&
+        isNamespaceLoaded("waldo")) {
+      registerS3method("compare_proxy", "mesh3d", 
+                       compare_proxy.mesh3d, 
+                       envir = asNamespace("waldo"))
+      registered <<- TRUE
+    }
+  }
+})
+
+compare_proxy.mesh3d <- function(x) {
+  for (n in names(x)) # Some elements are NULL, some are not there
+    if (is.null(x[[n]])) x[[n]] <- NULL
+  if (is.null(x$material) ||
+      length(x$material$color) < 2)
+    x$meshColor <- NULL  # It doesn't matter.
+  for (n in c("ip", "is", "it", "ib"))
+    if (!is.null(x[[n]]))
+      x[[n]] <- as.numeric(x[[n]])
+  x$primitivetype <- NULL # Not used since before 0.100.x
+  x[sort(names(x))]
 }
 
 #
@@ -71,6 +109,10 @@ tmesh3d <- function( vertices, indices, homogeneous=TRUE, material=NULL, normals
   meshColor <- match.arg(meshColor)
   if (is.null(dim(vertices)))
     vertices <- matrix(vertices, nrow = if (homogeneous) 4 else 3)
+  
+  # For back-compatibility:
+  colnames(indices) <- NULL   
+  
   mesh3d(vertices = vertices, triangles = indices,
          material = material, normals = normals, texcoords = texcoords,
          meshColor = meshColor)
@@ -90,6 +132,9 @@ qmesh3d <- function( vertices, indices, homogeneous=TRUE, material=NULL, normals
   
   if (is.null(dim(vertices)))
     vertices <- matrix(vertices, nrow = if (homogeneous) 4 else 3)
+  
+  # For back-compatibility
+  colnames(indices) <- NULL   
   
   mesh3d(vertices = vertices, quads = indices,
          material = material, normals = normals, texcoords = texcoords,
@@ -381,20 +426,16 @@ print.mesh3d <- function(x, prefix = "", ...) {
       ".\n", sep = "")
 }
 
+match_names <- function(x, reference) {
+  if (!is.null(x))
+    structure(x[names(reference)], class = class(x))
+}
+
 # Compare old and new meshes
 all.equal.mesh3d <- function(target, current, ...) {
-  mcs <- c(target$meshColor, current$meshColor)
-  target$meshColor <- NULL
-  current$meshColor <- NULL
-  result <- all.equal.list(target, current, ...)
-  if (isTRUE(result)) 
-    result <- character()
-  mctest <- is.null(mcs) ||
-            (length(mcs) == 1 && mcs == "vertices") ||
-            (length(mcs) == 2 && mcs[1] == mcs[2])
-  if (!mctest)
-    result <- c(result, "meshColor difference")
-  if (!length(result))
-    result <- TRUE
-  result
+  if (inherits(current, "mesh3d"))
+    all.equal(compare_proxy.mesh3d(target), 
+              compare_proxy.mesh3d(current), ...)
+  else
+    "'current' is not a mesh3d object"
 }

NAMESPACE
@@ -5,7 +5,8 @@ export(.check3d,
   aspect3d, axes3d, axis3d, box3d,
   bbox3d, bg3d, bgplot3d, 
 	clear3d, clearSubsceneList, clipplanes3d, clipplaneSlider, 
-	clipMesh3d, clipObj3d, close3d, contourLines3d,
+	clipMesh3d, clipObj3d, close3d, compare_proxy.mesh3d,
+  contourLines3d,
 	cube3d, cuboctahedron3d, cur3d, 
   currentSubscene3d, cylinder3d,
   decorate3d, deform.mesh3d, delFromSubscene3d, divide.mesh3d, dodecahedron3d, dot3d, drape3d, ellipse3d, extrude3d,

R/zzz.R
@@ -94,6 +94,8 @@
                       mono = c("Courier", "Courier New")))
   }
   
+  register_compare_proxy()
+  
   .rglEnv$subsceneList <- NULL
 
   # Workaround for incompatibility with quartz device

man/all.equal.mesh3d.Rd
@@ -1,0 +1,52 @@
+\name{all.equal.mesh3d}
+\alias{all.equal.mesh3d}
+\alias{compare_proxy.mesh3d}
+\title{
+Compare mesh3d objects in a meaningful way.
+}
+\description{
+These functions allow comparison of mesh3d objects, ignoring
+irrelevant differences.  
+
+\code{compare_proxy.mesh3d} can function as a \code{compare_proxy} method for the
+\pkg{waldo} package, by stripping out \code{NULL} components
+and ordering other components alphabetically by name.
+
+\code{all.equal.mesh3d} compares mesh3d objects by using
+\code{compare_proxy.mesh3d} to standardize them, then
+using the regular \code{\link{all.equal}} function to
+compare them.
+}
+\usage{
+\method{all.equal}{mesh3d}(target, current, ...)
+compare_proxy.mesh3d(x)
+}
+%- maybe also 'usage' for other objects documented here.
+\arguments{
+  \item{target, current}{
+Two mesh3d objects to compare.  
+}
+  \item{x}{
+A single mesh3d object to standarize.
+}
+  \item{\dots}{
+Additional parameters to pass to \code{\link{all.equal}}.
+}
+}
+\value{
+\code{all.equal.mesh3d} returns \code{TRUE}, or a character
+vector describing (some of) the differences.
+
+\code{compare_proxy.mesh3d} returns a list containing the
+relevant components of the argument in alphabetical order.
+}
+\note{
+\pkg{waldo} is not an installation requirement for \pkg{rgl}
+and \pkg{rgl} will never cause it to be loaded.
+The \code{compare_proxy.mesh3d} function will only be 
+registered as a method for \code{waldo::compare_proxy}
+if you load \pkg{waldo} before \pkg{rgl}, as would normally 
+happen during testing using \pkg{testthat}, or if you load
+it before calling \code{\link{mesh3d}},
+as might happen if you are doing manual tests.
+}

commit 9e7fef3f371227d23d662049a23360ed791e47b2

Fix bug introduced by commit 2c76c311dda0311c8bccd1da0974b62794a58a58:  meshColor argument was lost.  Fixes issue #87.

DESCRIPTION
@@ -1,5 +1,5 @@
 Package: rgl
-Version: 0.106.6
+Version: 0.106.7
 Title: 3D Visualization Using OpenGL
 Authors@R: c(person("Duncan", "Murdoch", role = c("aut", "cre"),
                      email = "murdoch.duncan@gmail.com"),
@@ -46,3 +46,4 @@ BugReports: https://github.com/dmurdoch/rgl/issues
 VignetteBuilder: knitr
 Biarch: true
 Additional_repositories:  https://dmurdoch.github.io/drat
+Remotes: dmurdoch/htmlwidgets@rglpatch, r-lib/downlit, r-lib/pkgdown

R/mesh3d.R
@@ -56,7 +56,8 @@ mesh3d <- function( x, y = NULL, z = NULL, vertices,
     vb = vertices,
     material = .getMaterialArgs(material = material),
     normals = normals,
-    texcoords = texcoords
+    texcoords = texcoords,
+    meshColor = meshColor
   ) 
   
   if (!is.null(points))    object$ip <- ensureMatrix(points, 1)