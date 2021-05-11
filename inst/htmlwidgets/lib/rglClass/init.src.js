44ae0dbe12006fab9b0d874d0633baf145051f9f
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-05-11 11:28:50 -0400

    Adding sphereColors broke playwidget. (Fixes #102)

--- inst/htmlwidgets/lib/rglClass/init.src.js
+++ inst/htmlwidgets/lib/rglClass/init.src.js
@@ -474,8 +474,6 @@
             colors[i][3] = colors[i][3]*options.filteredFade;   /* default: completely hidden if filtered */
           }
     }  
-    if (obj.type === "spheres")
-      obj.sphereColors = colors;
     
     nc = obj.colorCount = colors.length;
     if (nc > 1) {
