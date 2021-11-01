2b841d620b59128ba4a2da28dd751fedff81ec6a
Duncan Murdoch <murdoch.duncan@gmail.com>
2021-11-01 07:38:27 -0400

    Add test for issue 154

--- tests/indices.R
+++ tests/indices.R
@@ -8,3 +8,12 @@ open3d()
 text3d(t(tet$vb[1:3,]), text=1:4)
 triangles3d(t(tet$vb[1:3,]), indices = c(1,2,3,1,4,2,1,3,4,2,4,3),
             col = "red")
+
+# This displayed a triangle for the red quad (issue #154)
+
+quad <- cbind(x = c(-1, 1, 1, -1),
+              y = c( 0, 0, 0, 0),
+              z = c(-1, -1, 1, 1))/2
+open3d()
+triangles3d(quad, alpha = 0.5, col = "red", indices=c(1,2,3,1,3,4))
+triangles3d(quad+1, col = "blue", indices=c(1,2,3,1,3,4))
