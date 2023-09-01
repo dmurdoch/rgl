8ebdc0406eb4a95c8f007c704db374ac3d57ff21
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2023-09-01 14:50:22 +1000

    Passing cex argument to plotmath3d in text3d

--- R/r3d.rgl.R
+++ R/r3d.rgl.R
@@ -496,7 +496,7 @@ text3d      <- function(x, y = NULL, z = NULL,
 			...) {
   if (usePlotmath) 
     return(plotmath3d(x = x, y = y, z = z, text = texts, adj = adj, 
-                      pos = pos, offset = offset, ...))
+                      pos = pos, offset = offset, cex = cex, ...))
   .check3d(); save <- material3d(); on.exit(material3d(save))
   do.call("rgl.material0", .fixMaterialArgs(..., Params = save))
   # Force evaluation
