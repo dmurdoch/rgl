e1fb975f7cf1ea2aa391bad919b6f9db3fe76d83
Duncan Murdoch <murdoch.duncan@gmail.com>
2024-03-05 05:16:01 -0500

    Another typo!

--- vignettes/setup.R
+++ vignettes/setup.R
@@ -11,7 +11,7 @@ if (!requireNamespace("rmarkdown", quietly = TRUE)) {
 # If Pandoc is not installed, the output format won't be set
 # knitr needs this; see https://github.com/rstudio/markdown/issues/115
 if (is.null(knitr::opts_knit$get("rmarkdown.output.format"))){
-  knitr::opts_knit$set(rmarkdown.output.format = "html")
+  knitr::opts_knit$set(rmarkdown.pandoc.to = "html")
 }
   
 # knitr::opts_chunk$set(snapshot = TRUE)  # for snapshots instead of dynamic
